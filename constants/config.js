deed147f618f90d36b29d8ae9801572ed22af746
Mayank-xd-dev <mkmanav906@gmail.com>
2025-06-08 10:41:26 +0530

    fixed

--- constants/config.js
+++ constants/config.js
@@ -2,12 +2,8 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    process.env.CLIENT_URL,
+    "https://wechat-mocha.vercel.app", // ✅ replace this with your actual Vercel URL
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
-
-const CHATTU_TOKEN = "chattu-token";
-
-export { corsOptions, CHATTU_TOKEN };
